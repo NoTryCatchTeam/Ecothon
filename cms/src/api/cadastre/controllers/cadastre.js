fce30468cba42b2c977aa84310f87fad8b7d06c6
swamp1820 <swamp1820@yandex.ru>
2024-09-18 19:15:59 +0300

    add parks

---
+++ cms/src/api/cadastre/controllers/cadastre.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cadastre controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cadastre.cadastre');
