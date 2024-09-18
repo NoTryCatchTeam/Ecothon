62c69ef5f01f7bbf8f9651e0ccf2409c79599c19
swamp1820 <swamp1820@yandex.ru>
2024-09-18 12:10:39 +0300

    test ci

---
+++ cms/src/api/rb-object/controllers/rb-object.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * rb-object controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::rb-object.rb-object');
